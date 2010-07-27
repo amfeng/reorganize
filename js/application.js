f5bd1b06f81bf1cda79d1e9a625b72cce77726c6
Amber Feng <amber.feng@gmail.com>
2010-07-27 21:45:20 +0000

    Field Validation
    git-svn-id: http://svn.amberfeng.com/reorganize@3 6e6532d1-2a98-4da0-aa95-d455688ab8d1

--- js/application.js
+++ js/application.js
@@ -8,9 +8,17 @@ $(function(){
 	
 	createRoom(120, 120);
 	
+	$('#room-height').attr('value', parseInt($('.room').css('height'))/factor);
+	$('#room-width').attr('value', parseInt($('.room').css('width'))/factor);
+	
+	$('.error').html("Please enter valid dimensions.").hide();
 	
 	$('#room-submit').click(function(){
-		createRoom(parseInt($('#room-width')[0].value), parseInt($('#room-height')[0].value));
+		width = parseInt($('#room-width')[0].value);
+		height = parseInt($('#room-height')[0].value);
+		
+		createRoom(width, height);
+		
 	});
 	
 	$('#rect-submit').click(function(){
@@ -21,9 +29,6 @@ $(function(){
 		createDoor($('#door-select')[0].value, parseInt($('#door-size')[0].value))
 	});
 	
-	$('#room-height').attr('value', parseInt($('.room').css('height'))/factor);
-	$('#room-width').attr('value', parseInt($('.room').css('width'))/factor);
-	
 	$('#canvas').mouseleave(function(){
 		$('.float-info').hide();
 	});
@@ -31,146 +36,186 @@ $(function(){
 });
 
 function createRoom(x, y){
-	$('#canvas').html("");
-	$('#canvas').append("<div class='room'></div>");
-	$('#canvas > .room').css('height', y*factor).css('width', x*factor);
-	$('#canvas').append("<div class='clear'></div>");
+	if(x > 0 && y > 0){
+		$('#room-error').hide();
+		
+		$('#canvas').html("");
+		$('#canvas').append("<div class='room'></div>");
+		$('#canvas > .room').css('height', y*factor).css('width', x*factor);
+		$('#canvas').append("<div class='clear'></div>");
+	} else{
+		$('#room-error').show();
+	}
+	
 }
 
 function createRect(x, y){
-	numRect++;
-	$('.room').append("<div class='rect-margin' id='rect-" + numRect + "'></div>");
+	if(validateDimensions(x, y)){
+		$('#rect-error').hide();
+		
+		numRect++;
+		$('.room').append("<div class='rect-margin' id='rect-" + numRect + "'></div>");
 	
-	currRect = $('.room > #rect-' + numRect).css('position', 'absolute');
-	currRect.append("<div class='rect'></div>");
-	currRect.children('.rect').append("<div class='info'></div>");
+		currRect = $('.room > #rect-' + numRect).css('position', 'absolute');
+		currRect.append("<div class='rect'></div>");
+		currRect.children('.rect').append("<div class='info'></div>");
 	
-	currRect.css('height', y*factor).css('width', x*factor).css('padding', margin);
-	currRect.draggable({ 
-		containment: 'parent',
-		grid: [factor, factor],
-		snap: true,
-		snapMode: 'outer',
-		stack: '.rect-margin'
-	});
-	
-	currInfo = currRect.find('.info');
-	currInfo.append("<div class='delete'></div>");
-	currInfo.append("<div class='rotate'></div>");
-	currInfo.append("<div class='dim'><span class='width'>" + formatDim(parseInt(currRect.css('width'))) + "</span>x<span class='height'>" + formatDim(parseInt(currRect.css('height'))) + "</span></div>");
-	
-	/*$('.rect-margin').resizable({
-		containment: 'parent',
-		resize: function(){
-			rectContainer = $(this)
-			rectContainer.find('.width').html(formatDim(parseInt(rectContainer.css('width'))));
-			rectContainer.find('.height').html(formatDim(parseInt(rectContainer.css('height'))));
-		}
-	});*/
-	$('.rect-margin').hover(function(){
-		$('.float-info').hide();
-		rectContainer = $(this);
-		rectContainer.find('.info').show();
-	
-		rectContainer.find('.rotate').unbind('click').click(function(){
-			rotate(rectContainer);
+		currRect.css('height', y*factor).css('width', x*factor).css('padding', margin);
+		currRect.draggable({ 
+			containment: 'parent',
+			grid: [factor, factor],
+			snap: true,
+			snapMode: 'outer',
+			stack: '.rect-margin'
 		});
-		rectContainer.find('.delete').click(function(){
-			rectContainer.remove();
+	
+		currInfo = currRect.find('.info');
+		currInfo.append("<div class='delete'></div>");
+		currInfo.append("<div class='rotate'></div>");
+		currInfo.append("<div class='dim'><span class='width'>" + formatDim(parseInt(currRect.css('width'))) + "</span>x<span class='height'>" + formatDim(parseInt(currRect.css('height'))) + "</span></div>");
+	
+		/*$('.rect-margin').resizable({
+			containment: 'parent',
+			resize: function(){
+				rectContainer = $(this)
+				rectContainer.find('.width').html(formatDim(parseInt(rectContainer.css('width'))));
+				rectContainer.find('.height').html(formatDim(parseInt(rectContainer.css('height'))));
+			}
+		});*/
+		$('.rect-margin').hover(function(){
+			$('.float-info').hide();
+			rectContainer = $(this);
+			rectContainer.find('.info').show();
+	
+			rectContainer.find('.rotate').unbind('click').click(function(){
+				rotate(rectContainer);
+			});
+			rectContainer.find('.delete').click(function(){
+				rectContainer.remove();
+			});
+		}, function(){
+			$(this).find('.info').hide();
 		});
-	}, function(){
-		$(this).find('.info').hide();
-	});
+	} else{
+		$('#rect-error').show();
+	}
 
 }
 
 function createDoor(wall, size){
-	numDoor++;
-	$('.room').append("<div class='door " + wall + "'  id='door-" + numDoor + "'></div>");
-	currDoor = $('.room > #door-' + numDoor).css('position', 'absolute');
+	var validated;
 	
-	$('#canvas').append($("<div class='float-info' id='door-" + numDoor + "-info'></div>").append("<div class='delete'></div>"));
-	
-	$('.door').hover(function(){
-		$('.float-info').hide();
-		currDoor = $(this)
-		currInfo = resetInfoPosition(currDoor, currDoor.attr('class').split(' ')[1]).show();
-		currInfo.children('.delete').click(function(){
-			currDoor.remove();
-			$(this).parent().remove();
-		});
-	}, function(){
-	});
-	
-	function resetInfoPosition(ele, side){
-		if(side == 'top' || side == 'left') return $('#' + ele.attr('id') + '-info').css('top', ele.offset().top).css('left', ele.offset().left);
-		else if(side == 'right') return $('#' + ele.attr('id') + '-info').css('top', ele.offset().top).css('left', ele.offset().left + 6); //right
-		return $('#' + ele.attr('id') + '-info').css('top', ele.offset().top + 6).css('left', ele.offset().left); //bottom
+	if(wall == 'top' || wall == 'bottom'){
+		validated = validateDimensions(size, 1);
+	} else if(wall == 'left' || wall == 'bottom'){
+		validated = validateDimensions(1, size);
+	} else{
+		validated = false;
 	}
 	
-	switch(wall){
+	if(validated){
+		$('#door-error').hide();
 		
-		case 'top':	
-			currDoor.css('width', size*factor).css('height', 22).css('marginTop', -16);
-			currDoor.draggable({ 
-				containment: 'parent',
-				grid: [factor, factor],
-				axis: 'x',
-				drag: function() {
-					resetInfoPosition($(this), wall);
-				},
-				stop: function() {
-					resetInfoPosition($(this), wall);
-				}
+		numDoor++;
+		$('.room').append("<div class='door " + wall + "'  id='door-" + numDoor + "'></div>");
+		currDoor = $('.room > #door-' + numDoor).css('position', 'absolute');
+	
+		$('#canvas').append($("<div class='float-info' id='door-" + numDoor + "-info'></div>").append("<div class='delete'></div>"));
+	
+		$('.door').hover(function(){
+			$('.float-info').hide();
+			currDoor = $(this)
+			currInfo = resetInfoPosition(currDoor, currDoor.attr('class').split(' ')[1]).show();
+			currInfo.children('.delete').click(function(){
+				currDoor.remove();
+				$(this).parent().remove();
 			});
-			break;
-		case 'left':
-			currDoor.css('height', size*factor).css('width', 22).css('marginLeft', -16);
-			currDoor.draggable({ 
-				containment: 'parent',
-				grid: [factor, factor],
-				axis: 'y',
-				drag: function() {
-					resetInfoPosition($(this), wall);
-				},
-				stop: function() {
-					resetInfoPosition($(this), wall);
-				}
-			});
-			break;
-		case 'right':
-			currDoor.css('height', size*factor).css('width', 22).css('marginRight', -16).css('right', 0);
-			currDoor.draggable({ 
-				containment: 'parent',
-				grid: [factor, factor],
-				axis: 'y',
-				drag: function() {
-					resetInfoPosition($(this), wall);
-				},
-				stop: function() {
-					resetInfoPosition($(this), wall);
-				}
-			});
-			break;
-		case 'bottom':
-			currDoor.css('width', size*factor).css('height', 22).css('marginBottom', -16).css('bottom', 0);
-			currDoor.draggable({ 
-				containment: 'parent',
-				grid: [factor, factor],
-				axis: 'x',
-				drag: function() {
-					resetInfoPosition($(this), wall);
-				},
-				stop: function() {
-					resetInfoPosition($(this), wall);
-				}
-			});
-			break;
-		default:
-			alert("Something went wrong!");
-			numDoor--;
-			break;
+		}, function(){
+		});
+	
+		switch(wall){
+		
+			case 'top':	
+				currDoor.css('width', size*factor).css('height', 22).css('marginTop', -16);
+				currDoor.draggable({ 
+					containment: 'parent',
+					grid: [factor, factor],
+					axis: 'x',
+					drag: function() {
+						resetInfoPosition($(this), wall);
+					},
+					stop: function() {
+						resetInfoPosition($(this), wall);
+					}
+				});
+				break;
+			case 'left':
+				currDoor.css('height', size*factor).css('width', 22).css('marginLeft', -16);
+				currDoor.draggable({ 
+					containment: 'parent',
+					grid: [factor, factor],
+					axis: 'y',
+					drag: function() {
+						resetInfoPosition($(this), wall);
+					},
+					stop: function() {
+						resetInfoPosition($(this), wall);
+					}
+				});
+				break;
+			case 'right':
+				currDoor.css('height', size*factor).css('width', 22).css('marginRight', -16).css('right', 0);
+				currDoor.draggable({ 
+					containment: 'parent',
+					grid: [factor, factor],
+					axis: 'y',
+					drag: function() {
+						resetInfoPosition($(this), wall);
+					},
+					stop: function() {
+						resetInfoPosition($(this), wall);
+					}
+				});
+				break;
+			case 'bottom':
+				currDoor.css('width', size*factor).css('height', 22).css('marginBottom', -16).css('bottom', 0);
+				currDoor.draggable({ 
+					containment: 'parent',
+					grid: [factor, factor],
+					axis: 'x',
+					drag: function() {
+						resetInfoPosition($(this), wall);
+					},
+					stop: function() {
+						resetInfoPosition($(this), wall);
+					}
+				});
+				break;
+			default:
+				alert("Something went wrong!");
+				numDoor--;
+				break;
+		}
+	} else{
+		$('#door-error').show();
 	}
+}
+
+function validateDimensions(x, y){	
+	rWidth = parseInt($('.room').css('width'))/factor;
+	rHeight = parseInt($('.room').css('height'))/factor;
+	
+	if(x > rWidth || y > rHeight) return false;
+	
+	if(x > 0 && y > 0) return true;
+	
+	return false;
+}
+
+function resetInfoPosition(ele, side){
+	if(side == 'top' || side == 'left') return $('#' + ele.attr('id') + '-info').css('top', ele.offset().top).css('left', ele.offset().left);
+	else if(side == 'right') return $('#' + ele.attr('id') + '-info').css('top', ele.offset().top).css('left', ele.offset().left + 6); //right
+	return $('#' + ele.attr('id') + '-info').css('top', ele.offset().top + 6).css('left', ele.offset().left); //bottom
 }
 
 function rotate(ele){
